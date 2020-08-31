55c08896ce8f6fe21fa2641e87eca1726392a4e5
Bryan A. Hanson <hanson@depauw.edu>
2020-08-31 03:40:08 +0000

    version 0.4.176

--- inst/doc/ChemoSpec2D.R
+++ inst/doc/ChemoSpec2D.R
@@ -1,12 +1,14 @@
-## ----SetUp, echo = FALSE, eval = TRUE, results = "hide"----
+## ----SetUp, echo = FALSE, eval = TRUE, results = "hide"-----------------------
 
 # R options & configuration:
 set.seed(9)
 rm(list = ls())
 suppressMessages(library("knitr"))
-options(width =  30) # for pinp specifically (narrow cols)
 desc <- packageDescription("ChemoSpec2D")
 
 # Stuff specifically for knitr:
 #opts_chunk$set(eval = FALSE)
+
+## ----array, out.width = "60%", echo = FALSE, fig.cap = "Configuration of the data array. $I$, $J$ and $K$ are array indices; F2 and F1 are standard terms for NMR dimensions. The mode terminology is typically used in the PARAFAC literature.  This structure is also sometimes referred to as a data cube."----
+knitr::include_graphics("3Darray.png")
 
